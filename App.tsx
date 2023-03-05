75617f90055373941d0bc05112df5b38cd81455a
k-h-f <>
2023-03-05 19:24:37 +0000

    share option

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Button, Modal, Text, View} from 'react-native';
 
 function App(): JSX.Element {
   return (
-    <>
-      <Text>Windborn</Text>
-    </>
+    <Modal animationType="slide" transparent={true} visible={true}>
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        }}>
+        <View style={{padding: 20, backgroundColor: 'white'}}>
+          <Text style={{fontSize: 16, marginBottom: 10}}>Hello!</Text>
+          <Button title="Share" />
+          <Button title="Cancel" />
+        </View>
+      </View>
+    </Modal>
   );
 }
 
