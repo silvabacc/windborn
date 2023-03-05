4d4fc257baa94b91a1507d1c5f57328c2aeb9051
k-h-f <>
2023-03-05 18:17:33 +0000

    init

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <>
+      <Text>Windborn</Text>
+    </>
+  );
+}
+
+export default App;
