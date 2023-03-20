d4d9a1de714f0ffb245fbe63caca602945f5deae
silvabacc <>
2023-03-20 10:46:35 +0000

    add view to button

--- src/Common/Button.tsx
+++ src/Common/Button.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Pressable, StyleSheet, Text} from 'react-native';
+import {Pressable, StyleSheet, Text, View} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 
 interface ButtonProps {
@@ -11,10 +11,12 @@ interface ButtonProps {
 
 const Button: React.FC<ButtonProps> = ({title, icon, style, onPress}) => {
   return (
-    <Pressable style={style} testID="copy-clipboard-button" onPress={onPress}>
-      {icon && <Icon style={styles.icon} size={20} name="share-alt" />}
-      {title && <Text style={styles.text}>{title}</Text>}
-    </Pressable>
+    <View style={style}>
+      <Pressable testID="copy-clipboard-button" onPress={onPress}>
+        {icon && <Icon style={styles.icon} size={20} name="share-alt" />}
+        {title && <Text style={styles.text}>{title}</Text>}
+      </Pressable>
+    </View>
   );
 };
 
